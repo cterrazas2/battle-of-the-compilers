216c305a84504d479f4f9a2cac835d02edcfcea6
Chris Terrazas <cterrazas69@gmail.com>
2019-04-21 13:49:55 -0400

    scaffold for project

---
+++ DataStructures.h
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include <map>
+#include <stack>
+#include <queue>
+#include <chrono>
+
+template<typename T>
+void insert(T n, list<T>& lis)
+{
+  
+}
