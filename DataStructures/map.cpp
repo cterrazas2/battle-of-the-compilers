75d423708162cd440f91b16bb11c74843505e447
Rabia Akhtar <ra2805@columbia.edu>
2019-04-21 23:03:21 -0400

    Discarded first trial in map and added lookup

--- DataStructures/map.cpp
+++ DataStructures/map.cpp
@@ -6,8 +6,8 @@
 using namespace std;
 
 /* Fills a map with random elements and then randomly removes elements from the map. */
-void time_map(const std::vector<int>& rands, std::map<int, int>* map){
-	auto start = chrono::high_resolution_clock::now();
+void map_access(const std::vector<int>& rands, std::map<int, int>* map){
+	// Test insertion
 	for (const auto& elem: rands) {
 		(*map)[elem] = 0;
 	}
@@ -15,6 +15,7 @@ void time_map(const std::vector<int>& rands, std::map<int, int>* map){
 	default_random_engine real(0); 
 	uniform_int_distribution<int> distribution(0, map->size());
 
+	// Test deletion
 	for (int i=0; i < map->size();){
 		int pos = distribution(real);
 		if (pos < map->size()){
@@ -26,6 +27,26 @@ void time_map(const std::vector<int>& rands, std::map<int, int>* map){
 			++i;
 		}
 	}
+
+	for (const auto& elem: rands) {
+		(*map)[elem] = 0;
+	}
+
+	// Test map lookup 
+	for (int i=0; i < 10; i++){
+		int key = distribution(real);
+		auto search = map->find(key);
+		if (search != map->end()) {
+			(*map)[key] = 1;
+		}
+	}
+}
+
+/* Times map function */
+void time(void (*fn)(const std::vector<int>&, std::map<int, int>*), const std::vector<int>& rands, std::map<int, int>* map){
+	fn(rands, map);
+	auto start = chrono::high_resolution_clock::now();
+	fn(rands, map);
 	auto end = chrono::high_resolution_clock::now();
 	auto duration = chrono::duration_cast<chrono::milliseconds>(end - start).count();
 	cout << "map took time: " << duration << " milliseconds" << endl;
@@ -40,6 +61,6 @@ int main() {
 		rands.push_back(dist(re));
 	}
 	std::map<int, int> map;
-	time_map(rands, &map);
+	time(map_access, rands, &map);
 	return 0;
 }
