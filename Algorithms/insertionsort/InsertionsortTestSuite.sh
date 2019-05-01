38403555b6fb785e98c0480911ae5d56fb1de7e2
Emily Jin <emily@dyn-129-236-222-131.dyn.columbia.edu>
2019-05-01 13:00:48 -0400

    remove duplicate isort directory

--- Algorithms/insertionsort/InsertionsortTestSuite.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "g++ insertionsort file executing..."
-./insertionsort_gcc 2>buf.txt
-echo "clang++ insertionsort file executing..."
-./insertionsort_clang 2>buf.txt
-echo "icpc insertionsort file executing..."
-./insertionsort_intel 2>buf.txt
